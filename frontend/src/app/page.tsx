26282d74d1b1a96b21297adc27a62378ac9326ed
mrbeetrootemezthe23rdceo <eme.manninen@gmail.com>
2026-07-30 17:56:09 +0300

    made site black instead of white

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-1 flex-col items-center bg-zinc-50 dark:bg-black">
+    <div className="flex flex-1 flex-col items-center bg-background">
       <main className="flex w-full max-w-3xl flex-1 flex-col px-4 py-8">
         <MessageList messages={messages} />
 
